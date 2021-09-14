54ed5a9bcf28657633bfd3ae80a9f9f3b9be4e1f
Lucas Simão <oliveira.lucas20@gmail.com>
2021-09-13 23:37:13 -0300

    check play function created

--- app/src/main/java/com/lucasoliveirasimao/jokempo/MainActivity.kt
+++ app/src/main/java/com/lucasoliveirasimao/jokempo/MainActivity.kt
@@ -8,15 +8,17 @@ import android.view.animation.Animation
 import android.widget.Toast
 import android.view.View
 import com.lucasoliveirasimao.jokempo.databinding.ActivityMainBinding
-import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
-    private var disapper: Animation = AlphaAnimation(1.0F,0.0F)
+    private var disapper: Animation = AlphaAnimation(1.0F, 0.0F)
     private var appear: Animation = AlphaAnimation(0.0F, 1.0F)
 
     private lateinit var media: MediaPlayer
+
+    private var played1: Int = 0
+    private var played2: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -61,6 +63,7 @@ class MainActivity : AppCompatActivity() {
             }
 
             override fun onAnimationEnd(p0: Animation?) {
+                checkPlay()
                 binding.ivPlayer2.visibility = View.VISIBLE
             }
 
@@ -68,22 +71,37 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-    //TODO: criar a função de verifica a jogada
-
-
-
-    private fun drawEnemyGame(){
-        var random: Random
-        var numRandom: Int = random.nextInt(3)
-
-        when(numRandom){
-            0 -> binding.ivPlayer2.setImageResource(R.drawable.stone)
-            1 -> binding.ivPlayer2.setImageResource(R.drawable.paper)
-            2 -> binding.ivPlayer2.setImageResource(R.drawable.scissors)
+    private fun checkPlay() {
+        if (played1 == played2) {
+            Toast.makeText(this, "Empate!", Toast.LENGTH_SHORT).show()
+        }
+        if ((played1 == 1 && played2 == 3) || (played1 == 2 && played2 == 1) || (played1 == 3 && played2 == 2)) {
+            Toast.makeText(this, "Ganhei!", Toast.LENGTH_SHORT).show()
+        }
+        if ((played2 == 1 && played1 == 3) || (played2 == 2 && played1 == 1) || (played2 == 3 && played1 == 2)) {
+            Toast.makeText(this, "Perdi!", Toast.LENGTH_SHORT).show()
         }
     }
 
-    private fun playMusic(){
+    private fun drawEnemyGame() {
+
+        when ((0..2).random()) {
+            0 -> {
+                binding.ivPlayer2.setImageResource(R.drawable.stone)
+                played2 = 1
+            }
+            1 -> {
+                binding.ivPlayer2.setImageResource(R.drawable.paper)
+                played2 = 2
+            }
+            2 -> {
+                binding.ivPlayer2.setImageResource(R.drawable.scissors)
+                played2 = 3
+            }
+        }
+    }
+
+    private fun playMusic() {
         media = MediaPlayer.create(baseContext, R.raw.alex_play)
         media.start()
     }
@@ -93,10 +111,21 @@ class MainActivity : AppCompatActivity() {
 
         binding.ivPlayer1.scaleX = -1.0F
 
-        when(view.id){
-            R.id.iv_stone -> binding.ivPlayer1.setImageResource(R.drawable.stone)
-            R.id.iv_paper -> binding.ivPlayer1.setImageResource(R.drawable.paper)
-            R.id.iv_scissors -> binding.ivPlayer1.setImageResource(R.drawable.scissors)
+        when (view.id) {
+            R.id.iv_stone -> {
+                binding.ivPlayer1.setImageResource(R.drawable.stone)
+                played1 = 1
+            }
+
+            R.id.iv_paper -> {
+                binding.ivPlayer1.setImageResource(R.drawable.paper)
+                played1 = 2
+            }
+
+            R.id.iv_scissors -> {
+                binding.ivPlayer1.setImageResource(R.drawable.scissors)
+                played1 = 3
+            }
         }
 
         binding.ivPlayer2.startAnimation(disapper)
